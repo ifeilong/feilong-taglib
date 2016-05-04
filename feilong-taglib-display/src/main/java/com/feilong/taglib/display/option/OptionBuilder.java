b4eec0f0536abdce53eb8e5a4e17030d3c351c04
feilong <venusdrogon@163.com>
2016-05-04 16:15:49 +0800

    update javadoc

--- feilong-taglib-display/src/main/java/com/feilong/taglib/display/option/OptionBuilder.java
+++ feilong-taglib-display/src/main/java/com/feilong/taglib/display/option/OptionBuilder.java
@@ -40,6 +40,13 @@ public class OptionBuilder{
     /** 选中的字符串 <code>{@value}</code>. */
     private static final String SELECTED_STRING = " selected=\"selected\"";
 
+    /** Don't let anyone instantiate this class. */
+    private OptionBuilder(){
+        //AssertionError不是必须的. 但它可以避免不小心在类的内部调用构造器. 保证该类在任何情况下都不会被实例化.
+        //see 《Effective Java》 2nd
+        throw new AssertionError("No " + getClass().getName() + " instances for you!");
+    }
+
     /**
      * Builds the options.
      *
