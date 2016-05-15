c0a9adda348e6061e276652b27d654d985ec68c4
feilong <venusdrogon@163.com>
2016-05-15 15:19:58 +0800

    update javadoc

--- feilong-taglib-display/src/main/java/com/feilong/taglib/display/barcode/BarcodeTag.java
+++ feilong-taglib-display/src/main/java/com/feilong/taglib/display/barcode/BarcodeTag.java
@@ -51,9 +51,7 @@ public class BarcodeTag extends AbstractStartWriteContentTag{
     /** The Constant serialVersionUID. */
     private static final long serialVersionUID     = 7891625772743297912L;
 
-    /**
-     * 用来标识唯一的barcode,这样同一个页面如果出现不同的barcode不会冲突.
-     */
+    /** 用来标识唯一的barcode,这样同一个页面如果出现不同的barcode不会冲突. */
     private String            barcodeId;
 
     /** 生成二维码的内容,如果不设置默认是当前请求的url地址. */
