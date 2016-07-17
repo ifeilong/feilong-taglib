628bfd7af8c9e1214c3a06d885abe21a86f7fbad
feilong <venusdrogon@163.com>
2016-07-18 02:16:04 +0800

    call readToMap

--- feilong-taglib-display/src/main/java/com/feilong/taglib/display/option/OptionBuilder.java
+++ feilong-taglib-display/src/main/java/com/feilong/taglib/display/option/OptionBuilder.java
@@ -24,7 +24,7 @@ import com.feilong.taglib.display.TagCacheManager;
 import com.feilong.tools.slf4j.Slf4jUtil;
 
 import static com.feilong.core.Validator.isNotNullOrEmpty;
-import static com.feilong.core.util.ResourceBundleUtil.readPropertiesToMap;
+import static com.feilong.core.util.ResourceBundleUtil.readToMap;
 
 /**
  * 用来构造输出 option内容.
@@ -76,7 +76,7 @@ public class OptionBuilder{
         StringBuilder sb = new StringBuilder();
 
         //获得 key value map.
-        Map<String, String> map = readPropertiesToMap(optionParam.getBaseName(), optionParam.getLocale());
+        Map<String, String> map = readToMap(optionParam.getBaseName(), optionParam.getLocale());
         for (Map.Entry<String, String> entry : map.entrySet()){
             String option = buildOption(entry.getKey(), entry.getValue(), optionParam.getSelectedKey());
             sb.append(option).append(SystemUtils.LINE_SEPARATOR);
--- feilong-taglib-display/src/main/java/com/feilong/taglib/display/pager/PagerBuilder.java
+++ feilong-taglib-display/src/main/java/com/feilong/taglib/display/pager/PagerBuilder.java
@@ -43,7 +43,7 @@ import com.feilong.tools.velocity.VelocityUtil;
 import static com.feilong.core.Validator.isNotNullOrEmpty;
 import static com.feilong.core.Validator.isNullOrEmpty;
 import static com.feilong.core.util.MapUtil.newLinkedHashMap;
-import static com.feilong.core.util.ResourceBundleUtil.readPropertiesToMap;
+import static com.feilong.core.util.ResourceBundleUtil.readToMap;
 
 /**
  * 分页工具类.
@@ -470,7 +470,7 @@ public final class PagerBuilder{
      * @since 1.0.5
      */
     private static Map<String, String> buildI18nMap(PagerParams pagerParams){
-        return readPropertiesToMap(PagerConstants.I18N_FEILONG_PAGER, pagerParams.getLocale());
+        return readToMap(PagerConstants.I18N_FEILONG_PAGER, pagerParams.getLocale());
     }
 
     //****************************************************************************************************
