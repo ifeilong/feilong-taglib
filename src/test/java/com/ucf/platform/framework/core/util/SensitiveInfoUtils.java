88a801a03925b3bd57ee9e9d29b2154ea5bb8e8b
feilong <venusdrogon@163.com>
2018-06-10 18:54:18 +0800

    :memo: update javadoc

--- src/test/java/com/ucf/platform/framework/core/util/SensitiveInfoUtils.java
+++ src/test/java/com/ucf/platform/framework/core/util/SensitiveInfoUtils.java
@@ -1,23 +1,41 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ucf.platform.framework.core.util;
 
 import org.apache.commons.lang3.StringUtils;
 
 /**
+ * The Class SensitiveInfoUtils.
+ *
+ * @author liuwentao@ucfgroup.com
  * @Title: SensitiveInfoUtils.java
  * @Copyright: Copyright (c) 2011
  * @Description: <br>
  *               敏感信息屏蔽工具<br>
  * @Company: ucfgroup.com
  * @Created on 2014-11-18 下午3:50:15
- * @author liuwentao@ucfgroup.com
  */
 public final class SensitiveInfoUtils{
 
     /**
-     * [身份证号] 显示最后四位,其他隐藏。共计18位或者15位。<例子：*************5762>
-     * 
+     * [身份证号] 显示最后四位,其他隐藏。共计18位或者15位。<例子：*************5762>.
+     *
      * @param id
-     * @return
+     *            the id
+     * @return the string
      */
     public static String idCardNum(String id){
         if (StringUtils.isBlank(id)){
@@ -28,10 +46,11 @@ public final class SensitiveInfoUtils{
     }
 
     /**
-     * [固定电话] 后四位,其他隐藏<例子：****1234>
-     * 
+     * [固定电话] 后四位,其他隐藏<例子：****1234>.
+     *
      * @param num
-     * @return
+     *            the num
+     * @return the string
      */
     public static String fixedPhone(String num){
         if (StringUtils.isBlank(num)){
@@ -41,10 +60,11 @@ public final class SensitiveInfoUtils{
     }
 
     /**
-     * [银行卡号] 前六位,后四位,其他用星号隐藏每位1个星号<例子:6222600**********1234>
-     * 
+     * [银行卡号] 前六位,后四位,其他用星号隐藏每位1个星号<例子:6222600**********1234>.
+     *
      * @param cardNum
-     * @return
+     *            the card num
+     * @return the string
      */
     public static String bankCard(String cardNum){
         if (StringUtils.isBlank(cardNum)){
@@ -57,10 +77,11 @@ public final class SensitiveInfoUtils{
     }
 
     /**
-     * [公司开户银行联号] 公司开户银行联行号,显示前两位,其他用星号隐藏,每位1个星号<例子:12********>
-     * 
+     * [公司开户银行联号] 公司开户银行联行号,显示前两位,其他用星号隐藏,每位1个星号<例子:12********>.
+     *
      * @param code
-     * @return
+     *            the code
+     * @return the string
      */
     public static String cnapsCode(String code){
         if (StringUtils.isBlank(code)){
@@ -70,10 +91,11 @@ public final class SensitiveInfoUtils{
     }
 
     /**
-     * [银行卡有效期] 前1位,后1位,其他隐藏<例子:“0**6”>
-     * 
-     * @param num
-     * @return
+     * [银行卡有效期] 前1位,后1位,其他隐藏<例子:“0**6”>.
+     *
+     * @param date
+     *            the date
+     * @return the string
      */
     public static String cardValidDate(String date){
         if (StringUtils.isBlank(date)){
@@ -84,10 +106,11 @@ public final class SensitiveInfoUtils{
     }
 
     /**
-     * 全部隐藏
-     * 
-     * @param num
-     * @return
+     * 全部隐藏.
+     *
+     * @param date
+     *            the date
+     * @return the string
      */
     public static String all(String date){
         if (StringUtils.isBlank(date)){
@@ -96,42 +119,33 @@ public final class SensitiveInfoUtils{
         return StringUtils.repeat("*", StringUtils.length(date));
     }
 
+    /**
+     * The Enum SensitiveType.
+     */
     public static enum SensitiveType{
-        /**
-         * 中文名
-         */
+
+        /** 中文名. */
         CHINESE_NAME,
 
-        /**
-         * 身份证号
-         */
+        /** 身份证号. */
         ID_CARD,
-        /**
-         * 座机号
-         */
+
+        /** 座机号. */
         FIXED_PHONE,
-        /**
-         * 手机号
-         */
+
+        /** 手机号. */
         MOBILE_PHONE,
 
-        /**
-         * 银行卡
-         */
+        /** 银行卡. */
         BANK_CARD,
-        /**
-         * 公司开户银行联号
-         */
+
+        /** 公司开户银行联号. */
         CNAPS_CODE,
 
-        /**
-         * 银行卡有效期
-         */
+        /** 银行卡有效期. */
         BANK_CARD_DATE,
 
-        /**
-         * 全部隐藏
-         */
+        /** 全部隐藏. */
         ALL;
     }
 }
